8dbc686278a628dd4422e7740cd18304f162c45e
Rohan Shirsalkar <95482857+RohanShirsalkar@users.noreply.github.com>
2024-12-24 01:08:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: *,
+    origin:'*',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
